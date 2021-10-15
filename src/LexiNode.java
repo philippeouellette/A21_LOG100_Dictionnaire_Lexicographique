a736e7c0c1273add80c6a346c8d65a66a18fc558
Philippe Ouellette <philippeouellette@protonmail.com>
2021-10-15 18:58:31 -0400

    Added validation to not create a new child if another one with the same letter already exists

--- src/LexiNode.java
+++ src/LexiNode.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.jar.JarException;
+
+import com.sun.java.swing.plaf.motif.resources.motif;
 
 public class LexiNode {
 	char representative_letter;
@@ -7,22 +10,43 @@ public class LexiNode {
 	ArrayList<LexiNode> childs = new ArrayList<LexiNode>();
 	
 	public void add_word(String word, String definition){
+		System.out.print(this.representative_letter +"\n");
+		System.out.print(this.current_word +"\n");
+		System.out.print(this.definition +"\n");
 		if(word.length() > 1){
+			representative_letter = word.charAt(0);
 			LexiNode child = new LexiNode();
 			childs.add(child);
 			add_word(word, definition, word.substring(1));
 		}
 	}
-	public void add_word(String word, String definition, String subword){
+	private void add_word(String word, String definition, String subword){
 		representative_letter = subword.charAt(0);
-		if(word.length() > 1){
+		//If we already have a child with that representative_letter, we
+		//send the rest of the word to him.
+		for(LexiNode child: childs){
+			if(word.substring(1,2).equals(child.getRepresentative_letter()))
+				child.add_word(word, definition, subword.substring(1));
+		}
+		if(subword.length() > 1){
+			System.out.print(this.representative_letter +"\n");
+			System.out.print(this.current_word +"\n");
+			System.out.print(this.definition +"\n");
 			LexiNode child = new LexiNode();
 			childs.add(child);
-			add_word(word, definition, word.substring(1));
+			try{
+				child.add_word(word, definition, subword.substring(1));
+			}
+			catch(StringIndexOutOfBoundsException e){
+				System.out.println("Error: " + e);
+			}
 		}
-		else if (word.length() == 1) {
+		else if (subword.length() == 1) {
 			this.current_word = word;
 			this.definition = definition;
+			System.out.print(this.representative_letter +"\n");
+			System.out.print(this.current_word +"\n");
+			System.out.print(this.definition +"\n");
 		}
 	}
 	
@@ -40,5 +64,10 @@ public class LexiNode {
 	
 	public ArrayList<LexiNode> getChilds(){
 		return childs;
+	}
+	
+	public static void main(String[] args) {
+		LexiNode dictionnaire = new LexiNode();
+		dictionnaire.add_word("bien", "une definition");
 	}
 }
