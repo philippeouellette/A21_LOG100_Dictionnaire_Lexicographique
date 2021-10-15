579d96ed2ba535d6c53f2b89d28f090c65ebe1a8
Philippe Ouellette <Philippe.Ouellette@mdfcommerce.com>
2021-10-15 18:12:37 -0400

    Added the base of the recursive algorithm + getters for variables

--- src/LexiNode.java
+++ src/LexiNode.java
@@ -7,9 +7,38 @@ public class LexiNode {
 	ArrayList<LexiNode> childs = new ArrayList<LexiNode>();
 	
 	public void add_word(String word, String definition){
-		
+		if(word.length() > 1){
+			LexiNode child = new LexiNode();
+			childs.add(child);
+			add_word(word, definition, word.substring(1));
+		}
 	}
 	public void add_word(String word, String definition, String subword){
-		
+		representative_letter = subword.charAt(0);
+		if(word.length() > 1){
+			LexiNode child = new LexiNode();
+			childs.add(child);
+			add_word(word, definition, word.substring(1));
+		}
+		else if (word.length() == 1) {
+			this.current_word = word;
+			this.definition = definition;
+		}
+	}
+	
+	public char getRepresentative_letter(){
+		return representative_letter;
+	}
+	
+	public String getCurrent_word(){
+		return current_word;
+	}
+	
+	public String getDefinition(){
+		return definition;
+	}
+	
+	public ArrayList<LexiNode> getChilds(){
+		return childs;
 	}
 }
