f7c18a7fdd501fd4cdbdffedc17d774ef7f2567e
Philippe Ouellette <Philippe.Ouellette@mdfcommerce.com>
2021-10-15 17:56:38 -0400

    Class variables and definition of add_word function

--- src/LexiNode.java
+++ src/LexiNode.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
 
 public class LexiNode {
-
+	char representative_letter;
+	String current_word;
+	String definition;
+	ArrayList<LexiNode> childs = new ArrayList<LexiNode>();
+	
+	public void add_word(String word, String definition){
+		
+	}
+	public void add_word(String word, String definition, String subword){
+		
+	}
 }
